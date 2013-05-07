6aeab910702894fbf1a9dc74ddfa6e565274bbb5
Franclis Galdino <franclis.silva@dce.ufpb.br>
2013-05-06 23:35:52 -0300

    MiniTeste + 1ª Lista
    Implementaçao do MiniTeste e da Primeira lista de exercicios.

--- MiniTeste + Lista1/src/Colecao.java
+++ MiniTeste + Lista1/src/Colecao.java
@@ -2,31 +2,39 @@
 public class Colecao {
 	
 	private String elementos [];
-	private int size = 0;
+	private int size;
+	private int novoSize = size*2;
 	
 	public Colecao(int t){
 		elementos = new String[t];
+		size = 0;
 	}
 	
 	public void add(String e){
 		if(size==elementos.length){
 			aumetarList(e);
 		}		
-		elementos[size++]= e; //add o elemento na posiçao do size atual, depois incrementa		 
+		//add o elemento na posiçao do size atual, depois incrementa size
+		elementos[size++]= e; 		 
 	}
 	
 	private void aumetarList(String e){
-		String listAux [] = new String[size*2]; //cria uma nova lista com o dobro do tamanho
+		//cria uma nova lista com o dobro do tamanho
+		String listAux [] = new String[size*2]; 
 		for(int i =0 ; i<size; i++){
-			listAux[i] = elementos[i];//copia cada elemento da lista antiga e coloca na nova
+			//copia cada elemento da lista antiga e coloca na nova
+			listAux[i] = elementos[i];
 		}
-		elementos = listAux; // diz que antiga lista agora é a nova lista				
+		// diz que antiga lista agora é a nova lista
+		elementos = listAux; 				
 	}
 	
 	public String getElemento(int ps){
-		if(ps<0 || ps>elementos.length) 	throw new RuntimeException("Posicao invalida"); // se a posiçao for invalida lança um exerçao	
+		// se a posiçao for invalida lança um exerçao
+		if(ps<0 || ps>elementos.length) throw new CollectionException("Posicao invalida"); 	
 		
-		return elementos[ps]; // se nao lança a exception, retorna a string da posiçao passada no metodo
+		// se nao lança a exception, retorna a string da posiçao passada no metodo
+		return elementos[ps]; 
 	}
 	
 	
@@ -34,18 +42,18 @@ public class Colecao {
 		return this.size;
 	}	
 	
-	public void addInicio(String s){
+	public void addInicio(String e){
 		if(elementos.length !=0){
 			for(int i=size; i>0;i--)
 				elementos[i] = elementos[i-1];								
 		}
-		elementos[0]=s;		
+		elementos[0]=e;		
 		size++;	
 	}
 	
 	public void addPosicao(String s, int pos){
 		if(pos>size || pos<0){
-			throw new RuntimeException("Posicao invalida"); // se a posiçao for invalida lança um exerçao	
+			throw new CollectionException("Posicao invalida"); // se a posiçao for invalida lança um exerçao	
 		}
 		if(size!=0){
 			for(int i = size; i> pos; i--)
@@ -55,26 +63,25 @@ public class Colecao {
 		size++;		
 	}
 	
-	public void removerInicio(){
-		for(int i =0; i<size; i++){
+	public void removerInicio(){ 
+		// lança uma exception se a lista estiver vazia
+		if(size==0)
+			throw new CollectionException("Nao existe elemento na lista");
+					
+		for(int i =0; i<size; i++){			
 			elementos[i] = elementos [i+1];
-		}
-		--size;
-	}
-	
-	public void removerPosicao(int pos){		
-		if(pos>size || pos<0){
-			throw new RuntimeException("Posicao invalida"); // se a posiçao for invalida lança um exerçao	
-		}
-				
-		for(int i = pos;i<size ; i++){
-			elementos[i] = elementos[i+1];		
 		}
 		size--;
 	}
 	
-	
-	
-	
-	
+	public void removerPosicao(int pos){		
+		if(pos>size || pos<0)
+			// se a posiçao for invalida lança um exerçao
+			throw new CollectionException("Posicao invalida"); 
+						
+		for(int i = pos;i<size ; i++){
+			elementos[i] = elementos[i+1];		
+		}
+		size--;
+	}	
 }
--- MiniTeste + Lista1/src/MiniTeste.java
+++ MiniTeste + Lista1/src/MiniTeste.java
@@ -6,46 +6,45 @@ public class MiniTeste {
 
 		Colecao c = new Colecao(10);
 
-		for(int i = 1; i <= 15; i++){
-			c.add("E"+i);
-		}
-
-		for(int i = 0; i < c.getSize(); i++){
-			System.out.println(c.getElemento(i));
-		}
-		
+//		for(int i = 1; i <= 15; i++){
+//			c.add("E"+i);
+//		}
+//
+//		for(int i = 0; i < c.getSize(); i++){
+//			System.out.println(c.getElemento(i));
+//		}
+//		
 		// add elemento no inicio;
 		c.addInicio("a");
 		c.addInicio("b");
 		c.addInicio("c");
 		c.addInicio("d");		
 		for(int i = 0; i < c.getSize(); i++){
-			System.out.println(c.getElemento(i));
+			System.out.print(c.getElemento(i)+", ");
 		}
-		System.out.println("\n");		
+		System.out.println("Adicionou cada elemento no inicio\n");		
 		
 		
 		//add elemento na posiçao
-		c.addPosicao("e",2);	
+		c.addPosicao("e",3);	
 		for(int i = 0; i < c.getSize(); i++){
 			System.out.print(c.getElemento(i)+", ");
 		}		
-		System.out.println("\n");
+		System.out.println("adicionou o elemento 'e' na posiçao '3'\n");
 		
 		//remover elemento do inicio
 		c.removerInicio();
 		for(int i = 0; i < c.getSize(); i++){
 			System.out.print(c.getElemento(i)+", ");
 		}	
-		System.out.println("\n");
+		System.out.println("removeu o primeiro elemento da lista\n");
 		
 		//remover elemento da posicao
-		c.removerPosicao(2);
+		c.removerPosicao(3);
 		for(int i = 0; i < c.getSize(); i++){
 			System.out.print(c.getElemento(i)+", ");
 		}
-		
-
+		System.out.print("removeu o elemento da posicao '3' \n");
 	}
 
 }
